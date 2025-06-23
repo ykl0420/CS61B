085c25949904a338b29d00774906891ac7e5059e
ykl <2841688659@qq.com>
2025-06-23 18:41:36 +0800

    done with AList construction in bad strategy

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -5,23 +5,45 @@ import edu.princeton.cs.algs4.Stopwatch;
  * Created by hug.
  */
 public class TimeAList {
-    private static void printTimingTable(AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
-        System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
-        System.out.printf("------------------------------------------------------------\n");
-        for (int i = 0; i < Ns.size(); i += 1) {
-            int N = Ns.get(i);
-            double time = times.get(i);
-            int opCount = opCounts.get(i);
-            double timePerOp = time / opCount * 1e6;
-            System.out.printf("%12d %12.2f %12d %12.2f\n", N, time, opCount, timePerOp);
-        }
-    }
+	private static void printTimingTable(AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
+		System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
+		System.out.printf("------------------------------------------------------------\n");
+		for (int i = 0; i < Ns.size(); i += 1) {
+			int N = Ns.get(i);
+			double time = times.get(i);
+			int opCount = opCounts.get(i);
+			double timePerOp = time / opCount * 1e6;
+			System.out.printf("%12d %12.2f %12d %12.2f\n", N, time, opCount, timePerOp);
+		}
+	}
 
-    public static void main(String[] args) {
-        timeAListConstruction();
-    }
+	public static void main(String[] args) {
+		timeAListConstruction();
+	}
 
-    public static void timeAListConstruction() {
-        // TODO: YOUR CODE HERE
-    }
+	public static void timeAListConstruction() {
+		// YOUR CODE HERE
+		AList<Integer> Ns = new AList<>();
+		AList<Double> times = new AList<>();
+		AList<Integer> opCounts = new AList<>();
+		int N = 1000;
+		while(N <= 128000){
+			timeAListConstructionWithN(N,Ns,times,opCounts);
+			N *= 2;
+		}
+		printTimingTable(Ns,times,opCounts);
+	}
+	public static void timeAListConstructionWithN(int N,AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts){
+		Ns.addLast(N);
+		int opCount = 0;
+		AList<Integer> list = new AList<>();
+		Stopwatch sw = new Stopwatch();
+		for(int i = 0; i < N; i ++){
+			list.addLast(i);
+			opCount ++;
+		}
+		double timeInSeconds = sw.elapsedTime();
+		times.addLast(timeInSeconds);
+		opCounts.addLast(opCount);
+	}
 }
