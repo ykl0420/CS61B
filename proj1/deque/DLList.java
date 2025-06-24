d9476c5578d875cea1e0cb7b28d50c70a5ff47f4
ykl <2841688659@qq.com>
2025-06-24 19:59:39 +0800

    done with LinkedListDeque,passed all initial tests and not write own tests yet.

---
+++ proj1/deque/DLList.java
@@ -0,0 +1,125 @@
+package deque;
+
+import java.util.Iterator;
+
+public class DLList<T> implements Iterable<T>{
+
+	private Node sentinel;
+	int size;
+
+	private class Node{
+		T first;
+		Node next,pre;
+		public Node(){
+			first = null;
+			next = null;
+			pre = null;
+		}
+		public Node(T _first,Node  _next,Node _pre){
+			first = _first;
+			next = _next;
+			pre = _pre;
+		}
+		public Node(Node p){
+			first = p.first;
+			next = p.next;
+			pre = p.pre;
+		}
+	}
+
+	private class DLListIterator implements Iterator<T>{
+		Node p;
+		public DLListIterator(){
+			p = new Node(sentinel);
+		}
+		public boolean hasNext(){
+			return p.first != null;
+		}
+		public T next(){
+			T retVal = p.first;
+			p = p.next;
+			return retVal;
+		}
+	}
+
+	public DLList(){
+		size = 0;
+		sentinel = new Node();
+		sentinel.next = sentinel;
+		sentinel.pre = sentinel;
+	}
+
+	/** Adds an item of type T to the front of the deque.
+	 *  You can assume that item is never null. */
+	public void addFirst(T item){
+		size ++;
+		Node tmpNext = sentinel.next;
+		sentinel.next = new Node(item,tmpNext,sentinel);
+		tmpNext.pre = sentinel.next;
+	}
+
+	/** Adds an item of type T to the back of the deque.
+	 *  You can assume that item is never null. */
+	public void addLast(T item) {
+		size ++;
+		Node tmpPre = sentinel.pre;
+		sentinel.pre = new Node(item,sentinel,tmpPre);
+		tmpPre.next = sentinel.pre;
+	}
+	/** Returns true if deque is empty, false otherwise. */
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	/** Returns the number of items in the deque. */
+	public int size() {
+		return size;
+	}
+	/** Prints the items in the deque from first to last, separated by a space.
+	 *  Once all the items have been printed, print out a new line. */
+	public void print() {
+		for(T x : this) System.out.print(x + " ");
+		System.out.println();
+	}
+	/** Removes and returns the item at the front of the deque.
+	 *  If no such item exists, returns null. */
+	public T removeFirst() {
+		Node tmpNext = sentinel.next;
+		sentinel.next = sentinel.next.next;
+		if(tmpNext != sentinel) {
+			size --;
+			tmpNext.next = null;
+			tmpNext.pre = null;
+		}
+		sentinel.next.pre = sentinel;
+		return tmpNext.first;
+	}
+	/** Removes and returns the item at the back of the deque.
+	 *  If no such item exists, returns null. */
+	public T removeLast() {
+		Node tmpPre = sentinel.pre;
+		sentinel.pre = sentinel.pre.pre;
+		if(tmpPre != sentinel) {
+			size --;
+			tmpPre.pre = null;
+			tmpPre.next = null;
+		}
+		sentinel.pre.next = sentinel;
+		return tmpPre.first;
+	}
+
+	/** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+	 *  If no such item exists, returns null. Must not alter the deque! */
+	public T get(int index){
+		if(index > size) return null;
+		int indCount = 0;
+		for(T x : this){
+			indCount ++;
+			if(indCount == index) return x;
+		}
+		return null;
+	}
+
+	public Iterator<T> iterator(){
+		return new DLListIterator();
+	}
+}
---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,72 @@
+package deque;
+
+import java.util.Iterator;
+
+public class LinkedListDeque<T> implements Iterable<T>,Deque<T>{
+	private DLList<T> list;
+
+	public LinkedListDeque(){
+		list = new DLList<>();
+	}
+	/** Adds an item of type T to the front of the deque.
+	 *  You can assume that item is never null. */
+	public void addFirst(T item){
+		list.addFirst(item);
+	}
+	/** Adds an item of type T to the back of the deque.
+	 *  You can assume that item is never null. */
+	public void addLast(T item){
+		list.addLast(item);
+	}
+	/** Returns true if deque is empty, false otherwise. */
+	public boolean isEmpty(){
+		return list.isEmpty();
+	}
+	/** Returns the number of items in the deque. */
+	public int size(){
+		return list.size();
+	}
+	/** Prints the items in the deque from first to last, separated by a space.
+	 *  Once all the items have been printed, print out a new line. */
+	public void printDeque(){
+		list.print();
+	}
+	/** Removes and returns the item at the front of the deque.
+	 *  If no such item exists, returns null. */
+	public T removeFirst(){
+		return list.removeFirst();
+	}
+	/** Removes and returns the item at the back of the deque.
+	 *  If no such item exists, returns null. */
+	public T removeLast(){
+		return list.removeLast();
+	}
+	/** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+	 *  If no such item exists, returns null. Must not alter the deque! */
+	public T get(int index){
+		return list.get(index);
+	}
+
+	/** The Deque objects we’ll make are iterable (i.e. Iterable<T>); so we must provide this method to return an iterator. */
+	public Iterator<T> iterator(){
+		return list.iterator();
+	}
+
+	/** Returns whether or not the parameter o is equal to the Deque.
+	 *  o is considered equal if it is a Deque and if it contains the same contents
+	 *  (as goverened by the generic T’s equals method) in the same order.
+	 *  (ADDED 2/12: You’ll need to use the instance of keywords for this. Read here for more information) */
+	public boolean equals(Object o){
+		if(o == null) return false;
+		if(o instanceof Deque) return false;
+		Deque<T> other = (Deque<T>)o;
+		if(this.size() != other.size()) return false;
+		Iterator<T> it1 = this.iterator(),it2 = other.iterator();
+		while(it1.hasNext()){
+			if(!it1.next().equals(it2.next())){
+				return false;
+			}
+		}
+		return true;
+	}
+}
