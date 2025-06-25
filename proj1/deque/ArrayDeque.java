c1976eee73bdb7596ed90adba39a7cc3a0be5d69
ykl <2841688659@qq.com>
2025-06-25 18:04:17 +0800

    added many @override

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -12,39 +12,47 @@ public class ArrayDeque<T> implements Iterable<T>,Deque<T> {
 
 	/** Adds an item of type T to the front of the deque.
 	 *  You can assume that item is never null. */
+	@Override
 	public void addFirst(T item) {
 		list.addFirst(item);
 	}
 	/** Adds an item of type T to the back of the deque.
 	 *  You can assume that item is never null. */
+	@Override
 	public void addLast(T item) {
 		list.addLast(item);
 	}
 	/** Returns true if deque is empty, false otherwise. */
+	@Override
 	public boolean isEmpty() {
 		return list.isEmpty();
 	}
 	/** Returns the number of items in the deque. */
+	@Override
 	public int size() {
 		return list.size();
 	}
 	/** Prints the items in the deque from first to last, separated by a space.
 	 *  Once all the items have been printed, print out a new line. */
+	@Override
 	public void printDeque() {
 		list.print();
 	}
 	/** Removes and returns the item at the front of the deque.
 	 *  If no such item exists, returns null. */
+	@Override
 	public T removeFirst() {
 		return list.removeFirst();
 	}
 	/** Removes and returns the item at the back of the deque.
 	 *  If no such item exists, returns null. */
+	@Override
 	public T removeLast() {
 		return list.removeLast();
 	}
 	/** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
 	 *  If no such item exists, returns null. Must not alter the deque! */
+	@Override
 	public T get(int index) {
 		return list.get(index);
 	}
@@ -53,6 +61,7 @@ public class ArrayDeque<T> implements Iterable<T>,Deque<T> {
 
 
 	 } so we must provide this method to return an iterator. */
+	@Override
 	public Iterator<T> iterator() {
 		return list.iterator();
 	}
@@ -61,6 +70,7 @@ public class ArrayDeque<T> implements Iterable<T>,Deque<T> {
 	 *  o is considered equal if it is a Deque and if it contains the same contents
 	 *  (as goverened by the generic T’s equals method) in the same order.
 	 *  (ADDED 2/12: You’ll need to use the instance of keywords for this. Read here for more information) */
+	@Override
 	public boolean equals(Object o) {
 		if(o == null) return false;
 		if(o instanceof Deque) return false;
--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -10,39 +10,47 @@ public class LinkedListDeque<T> implements Iterable<T>,Deque<T>{
 	}
 	/** Adds an item of type T to the front of the deque.
 	 *  You can assume that item is never null. */
+	@Override
 	public void addFirst(T item){
 		list.addFirst(item);
 	}
 	/** Adds an item of type T to the back of the deque.
 	 *  You can assume that item is never null. */
+	@Override
 	public void addLast(T item){
 		list.addLast(item);
 	}
 	/** Returns true if deque is empty, false otherwise. */
+	@Override
 	public boolean isEmpty(){
 		return list.isEmpty();
 	}
 	/** Returns the number of items in the deque. */
+	@Override
 	public int size(){
 		return list.size();
 	}
 	/** Prints the items in the deque from first to last, separated by a space.
 	 *  Once all the items have been printed, print out a new line. */
+	@Override
 	public void printDeque(){
 		list.print();
 	}
 	/** Removes and returns the item at the front of the deque.
 	 *  If no such item exists, returns null. */
+	@Override
 	public T removeFirst(){
 		return list.removeFirst();
 	}
 	/** Removes and returns the item at the back of the deque.
 	 *  If no such item exists, returns null. */
+	@Override
 	public T removeLast(){
 		return list.removeLast();
 	}
 	/** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
 	 *  If no such item exists, returns null. Must not alter the deque! */
+	@Override
 	public T get(int index){
 		return list.get(index);
 	}
@@ -52,6 +60,7 @@ public class LinkedListDeque<T> implements Iterable<T>,Deque<T>{
 	}
 
 	/** The Deque objects we’ll make are iterable (i.e. Iterable<T>); so we must provide this method to return an iterator. */
+	@Override
 	public Iterator<T> iterator(){
 		return list.iterator();
 	}
@@ -60,6 +69,7 @@ public class LinkedListDeque<T> implements Iterable<T>,Deque<T>{
 	 *  o is considered equal if it is a Deque and if it contains the same contents
 	 *  (as goverened by the generic T’s equals method) in the same order.
 	 *  (ADDED 2/12: You’ll need to use the instance of keywords for this. Read here for more information) */
+	@Override
 	public boolean equals(Object o){
 		if(o == null) return false;
 		if(o instanceof Deque) return false;
